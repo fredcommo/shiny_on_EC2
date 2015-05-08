8d8b0018be6aedc28bb0fa8b105b14e6f58287c4
frederic commo <fredcommo@MacBook-Pro-de-frederic.local>
2015-05-08 14:12:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+### Create a EC2 Ubuntu instance
+### ubuntu-trusty-14.04-amd64-server-20150325 (ami-47a23a30) works fine, and a t2-micro is enough for testing this.
+
+In the EC2 Instances page Description.
+- Click on the 'Security Groups'
+- Select the 'Inboud' tab
+- Check whether ports 80 and 3838 are opened. If not, add the following rules
+	- Type: Custom TCP rule, Protocole: TCP, Port range: 80, Source: anywhere 0.0.0.0/0
+	- Type: Custom TCP rule, Protocole: TCP, Port range: 3838, Source: anywhere 0.0.0.0/0
+
+
+### Change the .pem mode first
+chmod 400 <my-key-pair.pem>
+
+### Push the installShiny.sh file
+cd /myKeyFolder
+scp -i <my-key-pair.pem> path/to/local/installShiny.sh ubuntu@<Public_IP>:~
+
+### Connect to your EC2 instance
+ssh -i <my-key-pair.pem> ubuntu@<Public_IP>
+
+### Change mode, then run
+chmod +x installShiny.sh
+./installShiny.sh
+
+### Starting the server should return something like: shiny-server start/running, process <some process number>
+sudo start shiny-server
+
+### Back to your local machine, push your shiny app: AppFolder contains the ui.R and server.r (and any extra folder or file)
+scp -i <my-key-pair.pem> -r <path/to/local/AppFolder> ubuntu@<Public_IP>:/srv/shiny-server
+
+### Run the app in a browser
+http://<Public_IP>:3838/myApp
+
+### [An example with a very basic App](http://52.17.91.68:3838/basics/)
---
+++ installShiny.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo
+echo Updating Ubuntu packages...
+sleep 2
+sudo apt-get update
+
+echo
+echo Installing libcurl...
+sleep 2
+sudo apt-get install -y libcurl4-openssl-dev
+echo
+
+echo Installing R...
+sleep 2
+sudo apt-get install -y r-base
+sudo apt-get install -y r-base-dev
+echo
+
+echo Writting .Rprofile...
+sleep 2
+touch .Rprofile
+echo "
+local({
+  r <- getOption('repos')
+  r['CRAN'] <- 'http://cran.univ-lyon1.fr/'
+  options(repos = r)
+})
+
+.libPaths('/usr/lib/R/library')
+
+" > .Rprofile
+echo
+
+sudo chmod 777 /usr/lib/R/library
+
+echo Installing shiny...
+sleep 2
+sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/', lib='/usr/lib/R/library/')\""
+echo
+
+echo Installing shiny server...
+sudo apt-get install -y gdebi-core
+wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0.403-amd64.deb
+echo "y" | sudo gdebi shiny-server-1.3.0.403-amd64.deb
+sudo rm shiny-server-1.3.0.403-amd64.deb
+echo
+
+echo Installing supplementary R packages...
+sleep 2
+sudo su - -c "R -e \"install.packages(c('nplr'), repos='http://cran.univ-lyon1.fr/', lib='/usr/lib/R/library/')\""
+echo
+
+echo Changing chmod 777 /srv/shiny-server/ permissions...
+sudo chmod 777 /srv/shiny-server/
+echo Done.
+echo
+
+echo
+echo Everything is installed !
+echo
+
+echo "*******************************************************************************"
+echo "-------------------------------------------------------------------------------"
+echo
+echo You should be able to push your Apps folders to '/srv/shiny-server/'
+echo e.g. 'scp -i <my_key_pair.pem> </myShyniApp> ubuntu@<Public_IP>:/srv/shiny-server'
+echo
+echo "-------------------------------------------------------------------------------"
+echo "*******************************************************************************"
+echo
+
+
